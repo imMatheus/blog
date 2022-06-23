0b8e723d916b2bc09e42df855bc48f803d5b5509
imMatheus <matheusmendesbarata03@gmail.com>
2022-06-24 00:56:23 +0200

    now properly shows time to read for all articles

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -14,6 +14,12 @@ function generateLinkFromTitle(title: string) {
 }
 
 const ArticleCard: React.FC<ArticleCardProps> = ({ article, index }) => {
+	const stripedText = article.content.replaceAll(/\r?\n|\r/g, ' ') // removes all \t and \n from the text
+	const stripedTextLength = stripedText.length // gets length of the striped text
+	const matches = stripedText.match(/```(.*?)```/g) // finds all code snippets, all text surrounded by 2 ```
+	const matchesLength = matches?.reduce((sum, match) => sum + match.length, 0) || 0 // get the length of the code snippets
+	const timeToRead = Math.ceil((stripedTextLength - matchesLength * 0.5) / 500)
+
 	return (
 		<Link href={`/articles/${generateLinkFromTitle(article.fileName)}`} passHref={true}>
 			<a className="flex cursor-pointer gap-3">
@@ -21,7 +27,7 @@ const ArticleCard: React.FC<ArticleCardProps> = ({ article, index }) => {
 				<div className="space-y-1.5">
 					<p className="flex items-center gap-1.5 text-sm text-clr-text-grayed">
 						{dateFormat(article.date, 'mmmm dS, yyyy')}
-						<span className="block h-0.5 w-0.5 flex-shrink-0 rounded-full bg-clr-text-grayed"></span> 18 min read
+						<span className="block h-0.5 w-0.5 flex-shrink-0 rounded-full bg-clr-text-grayed"></span> {timeToRead} min read
 					</p>
 					<p className="text-base font-bold">{article.title}</p>
 					<div className="flex flex-wrap gap-1">
